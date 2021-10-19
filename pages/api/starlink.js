78d0f68de8433fe37ab17789ee211c01fcecf552
wonglok <yellowhappy831@gmail.com>
2021-10-19 16:33:03 +0800

    starlink

--- pages/api/starlink.js
+++ pages/api/starlink.js
@@ -3,7 +3,9 @@ import Cors from "cors";
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ["GET", "HEAD", "OPTIONS"],
+  origin: (origin, callback) => {
+    callback(null, [origin]);
+  },
 });
 
 // Helper method to wait for a middleware to execute before continuing
